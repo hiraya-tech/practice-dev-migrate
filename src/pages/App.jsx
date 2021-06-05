a1d07fa5d8940915375436e53611a3ff5bfe64f0
manny <e.manarang527@mybvc.ca>
2021-06-04 20:12:42 -0700

    added styling for visualization

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { observer } from "mobx-react";
 
+import Header from '../components/header/Header';
 import Menu from '../components/menu/Menu'
 
 import './App.css';
+
 
 export default observer(
   class App extends React.Component {
@@ -16,6 +18,7 @@ export default observer(
       
       return (
         <div className="app">
+          <Header />
           <Menu menuItems={menuItems} />
           <div className="app-content">
             Test
